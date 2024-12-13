2d01a9c92578968b3e8beaca10c7291152cb2f50
Bernardo <bgonzalezerramuspe@eest1chivilcoy.edu.ar>
2024-12-13 07:12:27 -0300

    Actualización de mensaje dinámico y generación de código QR
    - Se agregó una variable `message` para permitir la personalización del mensaje que se enviará por WhatsApp.
    - Se añadió la variable `link` para permitir la configuración dinámica del enlace de inscripción.
    - El enlace de WhatsApp se genera dinámicamente utilizando las nuevas variables.
    - Se implementó la generación dinámica del código QR utilizando la API de QR Server, con base en el enlace de WhatsApp.
    - Se mejoró la flexibilidad del código permitiendo modificar fácilmente el mensaje y enlace para futuras actualizaciones.

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const noButton = document.getElementById("noButton");
 const modal = document.getElementById("modal");
+const qrCode = document.getElementById("qrCode");
 
+// Variables configurables
+const message = '¡Hola! 👋📚 Te comparto el enlace de inscripción para la Escuela de Educación Secundaria Técnica Nº1 "Mariano Moreno" (EEST 1) 🏫. ¡No dejes pasar esta oportunidad para que tu hijo/a sea parte de nuestra comunidad educativa! 🚀✨';
+const link = "https://docs.google.com/forms/d/e/1FAIpQLSc3Tgu73jsDT3skc6wuSzIMlLdd1xFLp_fOSWtFoHh6Yt5Ilw/viewform";
+
+// Generar enlace dinámico
+const whatsappLink = `https://wa.me/?text=${encodeURIComponent(message)}%0A%0AHaz%20clic%20aqui%20para%20inscribirlo/a:%20${encodeURIComponent(link)}`;
+
+// Escuchar movimiento sobre el botón "No"
 noButton.addEventListener("mouseover", () => {
   const x =
     Math.random() * (window.innerWidth - noButton.offsetWidth - 20);
@@ -11,10 +20,13 @@ noButton.addEventListener("mouseover", () => {
   noButton.style.top = `${y}px`;
 });
 
+// Mostrar modal con QR
 function showModal() {
+  qrCode.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${encodeURIComponent(whatsappLink)}`;
   modal.style.display = "flex";
 }
 
+// Cerrar modal
 function closeModal() {
   modal.style.display = "none";
 }
